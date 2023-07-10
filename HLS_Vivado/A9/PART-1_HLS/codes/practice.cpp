19b536f7714a7d6712919836624eb8e7ea1b51a2
mygit-sampath-govardhan <18311A0425@sreenidhi.edu.in>
2023-07-10 16:59:19 +0530

    mini proj

--- HLS_Vivado/A9/PART-1_HLS/codes/practice.cpp
+++ HLS_Vivado/A9/PART-1_HLS/codes/practice.cpp
@@ -1,3 +1,4 @@
+//meth1//
 //header
 #ifndef _HEADER_H_
 #define _HEADER_H_
@@ -113,7 +114,111 @@ which is (or contains) an array with unknown size at compile time.
 INFO: [SYNCHK 200-10] 1 error(s), 0 warning(s).
 ERROR: [HLS 200-70] Synthesizability check failed.*/
 
+//meth2//
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#include <hls_stream.h>
+#include "ap_int.h"
+using namespace std;
+
+#define N 8      //length of input message
+#define y 25     //length of divisor(parity)
+#define x 800  //maximum length of crc
+
+typedef ap_uint<N> data;
+
+void crc24a(hls::stream<data>& input, hls::stream<data>& output);
+
+#endif
+#include "header.h"
 
 
 
+void crc24a(hls::stream<data>& input, hls::stream<data>& output) {
+
+#pragma HLS INTERFACE axis register both port=input
+#pragma HLS INTERFACE axis register both port=output
+
+	bool terminate = false;
+	ap_uint<1> crc[x] = {0};
+	ap_uint<1> rtc[x] = {0};
+	ap_uint<1> v = 0;
+	ap_uint<1> divisor[y] = {1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1};
+	data last;
+	int u = 0;
+
+loop1: while (!input.empty() && !terminate) {
+		data d = input.read();
+
+		if (d != 1) {
+		loop2: for (int i = 0; i < N; i++) {
+				crc[u + i] = d(i, i);
+			}
+			u = u + 8;
+		} else {
+			last = d;
+			terminate = true;
+		}
+	}
+
+	const int z = u + y - 1;
+
+loop3: for (int i = 0; i < u + y - 1; i++) {
+#pragma HLS PIPELINE II=1
+		crc[i] = (i < u) ? crc[i] : v;
+		rtc[i] = (i < u) ? crc[i] : v;
+	}
+
+loop4: for (int i = 0; i <= z - y; i++) {
+#pragma HLS PIPELINE II=1
+		if (crc[i] == 1 && last == 1) {
+		loop5: for (int j = 0; j < y; j++) {
+#pragma HLS UNROLL
+				int k = i + j;
+				crc[k] = crc[k] ^ divisor[j];
+			}
+		}
+	}
+
+	data o1;
+	int index;
+
+loop6: for (int i = 0; i < z; i = i + 8) {
+#pragma HLS PIPELINE
+	loop7: for (int j = 0; j < N; j++) {
+#pragma HLS UNROLL
+			index = i + j;
+			rtc[index] = crc[index] ^ rtc[index];
+			o1(j, j) = rtc[index];
+		}
+		output.write(o1);
+	}
+}
+#include "header.h"
+
+int main() {
+    hls::stream<data> a,b;
+    data last=1;
+
+   	       a.write(0b00010110);
+   	 //   a.write(0b10100110);
+   //	 a.write(0b10011110);
+   	       a.write(last);
+
+    crc24a(a, b);
+
+    cout << "CRC generator output : ";
+
+    while(!b.empty()){
+       data d = b.read();
+      for (int i = 0; i < N ; i++) {
+       	cout<< d(i,i);
+       }
+    }
+    cout<<endl;
+    return 0;
+}
+
+//synthesized but not giving timing report
 
